ebd7e152e785c5dae6c50ec920eb8956c7909a61
Husan-Muhiddinov <124786003+Husan-Muhiddinov@users.noreply.github.com>
2023-11-17 19:42:45 +0500

    first commit

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class User(AbstractUser):
+    profile = models.ImageField(upload_to='profile_images/')
+    cover = models.ImageField(upload_to='cover_images/')
+    followers = models.ManyToManyField('self', blank=True)
+    following = models.ManyToManyField('self', blank=True)
+    verified = models.BooleanField(default=False)
+    bio = models.CharField(max_length=150)
+    birth_of_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
