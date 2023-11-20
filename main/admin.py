c4ae2df37171775fdc384609ec05731d17963dc3
Husan-Muhiddinov <124786003+Husan-Muhiddinov@users.noreply.github.com>
2023-11-20 19:49:21 +0500

    first commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Post, Notification, Action
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ['body']
+
+
+admin.site.register([Action, Notification])
--- users/models.py
+++ users/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    profile = models.ImageField(upload_to='profile_images/')
-    cover = models.ImageField(upload_to='cover_images/')
+    profile = models.ImageField(upload_to='profile_images/', default="defaults/profile.png")
+    cover = models.ImageField(upload_to='cover_images/', default="defaults/cover.png")
     followers = models.ManyToManyField('self', blank=True)
     following = models.ManyToManyField('self', blank=True)
     verified = models.BooleanField(default=False)
-    bio = models.CharField(max_length=150)
+    bio = models.CharField(max_length=150, null=True, blank=True)
     birth_of_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
