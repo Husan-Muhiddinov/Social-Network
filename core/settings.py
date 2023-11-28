bee33d0ab83e736cbf81c3407f00786c0ee5841a
Husan-Muhiddinov <124786003+Husan-Muhiddinov@users.noreply.github.com>
2023-11-28 19:49:36 +0500

    first commit

--- core/settings.py
+++ core/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
-
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
